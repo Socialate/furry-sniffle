a12cc374f7bcf0c3ebf5f6e1677c76bbe428266e
Francis Tinashe Mudavanhu <1233925@students.wits.ac.za>
2017-08-22 05:49:42 +0200

    added entertainment class and appropriate test

---
+++ app/src/main/java/com/socialteinc/socialate/Entertainment.java
@@ -0,0 +1,102 @@
+package com.socialteinc.socialate;
+
+public class Entertainment {
+
+    private String name;
+    private String address;
+    private String latitude;
+    private String longitude;
+    private String description;
+    private String photoUrl;
+    private String uid;
+    private String author;
+    private String establishmentCategory;
+
+    public Entertainment() {
+    }
+
+    public Entertainment(String name, String address, String latitude, String longitude, String description, String photoUrl, String uid, String author, String establishmentCategory) {
+        this.name = name;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.description = description;
+        this.photoUrl = photoUrl;
+        this.uid = uid;
+        this.author = author;
+        this.establishmentCategory = establishmentCategory;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setUID(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUID() {
+        return uid;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setEstablishmentCategory(String establishmentcategory) {
+        this.establishmentCategory = establishmentcategory;
+    }
+
+    public String getEstablishmentCategory() {
+        return establishmentCategory;
+    }
+}
