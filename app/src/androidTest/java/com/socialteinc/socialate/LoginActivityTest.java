1667530b4d932b2fe5c978a30ef8c711ff2dbbf1
MusaRikhotso <1090294@students.wits.ac.za>
2017-08-24 15:38:42 +0200

    LoginActivity tests updated

--- app/src/androidTest/java/com/socialteinc/socialate/LoginActivityTest.java
+++ app/src/androidTest/java/com/socialteinc/socialate/LoginActivityTest.java
@@ -1,6 +1,7 @@
 package com.socialteinc.socialate;
 
 
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import org.junit.Rule;
@@ -9,20 +10,39 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNot.not;
 
 @RunWith(AndroidJUnit4.class)
 
 public class LoginActivityTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> main = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<LoginActivity> main = new ActivityTestRule<>(LoginActivity.class);
 
     @Test
     public void loginTestingWithWrongCredentials(){
-        onView(withId(R.id.email_field)).perform(typeText("zzz@gmail.com"), closeSoftKeyboard());
-        onView(withId(R.id.password_field)).perform(typeText("sandile"), closeSoftKeyboard());
+        onView(withId(R.id.email_field)).perform(typeText("invalid@socialate.com"), closeSoftKeyboard());
+        onView(withId(R.id.password_field)).perform(typeText("furry"), closeSoftKeyboard());
         onView(withId(R.id.SinginButton)).perform(click());
+        onView(withText("Logging In")).check(matches(isDisplayed()));
+    }
+
+
+    @Test
+    public void noValidEmailTest() throws InterruptedException {
+        onView(withId(R.id.email_field)).perform(typeText("invalidsocialate.com"), closeSoftKeyboard());
+        onView(withId(R.id.password_field)).perform(typeText("furry"), closeSoftKeyboard());
+        onView(withId(R.id.SinginButton)).perform(click());
+
+        Thread.sleep(3000);
+        onView(withText("Authentication failed. Please enter valid username/password")).inRoot(withDecorView(not(is(main.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+
     }
 
     @Test
