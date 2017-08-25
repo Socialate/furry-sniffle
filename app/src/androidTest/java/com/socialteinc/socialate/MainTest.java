1c58bafac0175bfd9cc5d4164a9a8f6e27cf1b6f
Francis Tinashe Mudavanhu <1233925@students.wits.ac.za>
2017-08-25 20:18:42 +0200

    updated main activity tests

--- app/src/androidTest/java/com/socialteinc/socialate/MainTest.java
+++ app/src/androidTest/java/com/socialteinc/socialate/MainTest.java
@@ -1,10 +1,21 @@
 package com.socialteinc.socialate;
 
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.intent.Intents.init;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
 
 @RunWith(AndroidJUnit4.class)
 public class MainTest {
@@ -13,20 +24,37 @@ public class MainTest {
     public ActivityTestRule<MainActivity> main = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void testAddEntertainment(){
-
-//        Intents.init();
-//        onView(withId(R.id.action_add_entertainment)).perform(click());
-//        intended(hasComponent(AddEntertainmentActivity.class.getName()));
-//        Intents.release();
-
+    public void testAddEntertainment() throws InterruptedException {
+        login();
+        Thread.sleep(5000);
+        init();
+        onView(withId(R.id.action_add_entertainment)).perform(click());
+        intended(hasComponent(AddEntertainmentActivity.class.getName()));
+        Intents.release();
+        onView(withId(R.id.entertainmentTitleEditText)).perform(closeSoftKeyboard());
+        onView(isRoot()).perform(ViewActions.pressBack());
+        logout();
     }
 
     @Test
-    public void testLogout(){
-//        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-//        onView(withText("Logout")).perform(click());
-//        intended(hasComponent(LoginActivity.class.getName()));
-//        Intents.release();
+    public void testLogout() throws InterruptedException {
+        login();
+        Thread.sleep(5000);
+        Intents.init();
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText("Logout")).perform(click());
+        intended(hasComponent(LoginActivity.class.getName()));
+        Intents.release();
+    }
+
+    public void login(){
+        onView(withId(R.id.email_field)).perform(typeText("joe@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.password_field)).perform(typeText("sandile"), closeSoftKeyboard());
+        onView(withId(R.id.SinginButton)).perform(click());
+    }
+
+    public void logout(){
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText("Logout")).perform(click());
     }
 }
