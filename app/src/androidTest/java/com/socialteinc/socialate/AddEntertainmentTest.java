29e0272eecb74656c3414f205603a0a6cd969d8c
Francis Tinashe Mudavanhu <1233925@students.wits.ac.za>
2017-08-27 23:20:32 +0200

    Added AddEntertaiment test class

---
+++ app/src/androidTest/java/com/socialteinc/socialate/AddEntertainmentTest.java
@@ -0,0 +1,21 @@
+package com.socialteinc.socialate;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class AddEntertainmentTest {
+
+    @Rule
+    public ActivityTestRule<AddEntertainmentActivity> rule = new ActivityTestRule<>(AddEntertainmentActivity.class);
+
+    @Test
+    public void testLaunchActivity(){
+
+    }
+
+}
