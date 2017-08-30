188d932b6724f870685fe20cfe2aa9d7b8433ba0
sandile11 <1236067@students.wits.ac.za>
2017-08-30 22:58:42 +0200

    Cleaned up test files.
    Signed-off-by: sandile11 <1236067@students.wits.ac.za>

---
+++ app/src/androidTest/java/com/socialteinc/socialate/ViewAddedAreaTest1.java
@@ -0,0 +1,53 @@
+package com.socialteinc.socialate;
+
+import android.content.Context;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.longClick;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+
+@RunWith(AndroidJUnit4.class)
+
+public class ViewAddedAreaTest1 {
+
+    @Rule
+    public ActivityTestRule<ViewAddedActivity> mActivityRule =
+            new ActivityTestRule<ViewAddedActivity>(ViewAddedActivity.class) {
+                @Override
+                protected Intent getActivityIntent() {
+                    Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+                    Intent intent = new Intent(targetContext, MainActivity.class);
+                    intent.putExtra("description_val", "Smoke House");
+                    intent.putExtra("address_val", "23 Furry-Sniffle Rd., Socialate, 2017");
+                    intent.putExtra("title_val", "Smoke House");
+                    intent.putExtra("owner_val", "EGC Group");
+                    Resources resources = InstrumentationRegistry.getTargetContext().getResources();
+                    intent.putExtra("imageUri", (Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + resources.getResourcePackageName(R.mipmap.ic_launcher) + '/' +
+                            resources.getResourceTypeName(R.mipmap.ic_launcher) + '/' +
+                            resources.getResourceEntryName(R.mipmap.ic_launcher))).toString());
+                    return intent;
+                }
+            };
+
+    @Test
+    public void ViewAreaBackButtonTest(){
+       onView(withId(R.id.ViewAddedAreaOwnerText)).check(matches(isDisplayed()));
+
+    }
+
+
+}
