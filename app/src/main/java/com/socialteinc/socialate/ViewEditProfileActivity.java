5f23245db934e8398a034cea02aa7fc3741664d2
MusaRikhotso <1090294@students.wits.ac.za>
2017-09-11 15:44:17 +0200

    View edit profile woriking properly again

--- app/src/main/java/com/socialteinc/socialate/ViewEditProfileActivity.java
+++ app/src/main/java/com/socialteinc/socialate/ViewEditProfileActivity.java
@@ -2,14 +2,12 @@ package com.socialteinc.socialate;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
