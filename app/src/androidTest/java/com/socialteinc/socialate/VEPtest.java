1385cf9d62d25e944cd95e931d1edfdc9efe98c2
sandile11 <1236067@students.wits.ac.za>
2017-09-17 00:10:51 +0200

    A few changes to the failing tests.
    Signed-off-by: sandile11 <1236067@students.wits.ac.za>

--- app/src/androidTest/java/com/socialteinc/socialate/VEPtest.java
+++ app/src/androidTest/java/com/socialteinc/socialate/VEPtest.java
@@ -1,9 +1,19 @@
 package com.socialteinc.socialate;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 import android.view.View;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,9 +29,19 @@ import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 @RunWith(AndroidJUnit4.class)
 
 public class VEPtest {
+    private FirebaseAuth mAuth;
 
     @Rule
     public ActivityTestRule<ViewEditProfileActivity> main = new ActivityTestRule<>(ViewEditProfileActivity.class);
+
+    @Before
+    public void login(){
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
+        mAuth.signInWithEmailAndPassword("joe@gmail.com", "sandile");
+
+    }
 
     @Test
     public void UiTest(){
