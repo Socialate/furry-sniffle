142e9f573e4b3773471016cece8a0e5fedaaf1d7
sandile11 <1236067@students.wits.ac.za>
2017-09-29 05:12:35 +0200

    results can now be viewed. Progress.
    Signed-off-by: sandile11 <1236067@students.wits.ac.za>

--- app/src/main/java/com/socialteinc/socialate/MainActivity.java
+++ app/src/main/java/com/socialteinc/socialate/MainActivity.java
@@ -238,10 +238,11 @@ public class MainActivity extends AppCompatActivity {
         // Get the SearchView and set the searchable configuration
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+
         // Assumes current activity is the searchable activity
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-
+        searchView.setMaxWidth(Integer.MAX_VALUE);
 
         return true;
     }
