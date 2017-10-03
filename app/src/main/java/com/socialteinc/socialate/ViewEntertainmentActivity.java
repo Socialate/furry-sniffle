9ca725950efb496c7d383f5463be674e32233512
MusaRikhotso <1090294@students.wits.ac.za>
2017-10-03 16:21:30 +0200

    Users can now view all comments

--- app/src/main/java/com/socialteinc/socialate/ViewEntertainmentActivity.java
+++ app/src/main/java/com/socialteinc/socialate/ViewEntertainmentActivity.java
@@ -6,18 +6,28 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.*;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.*;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.ListIterator;
 
 public class ViewEntertainmentActivity extends AppCompatActivity {
@@ -47,10 +57,15 @@ public class ViewEntertainmentActivity extends AppCompatActivity {
     private DatabaseReference mUserDatabaseReference;
     private DatabaseReference mLikesDatabaseReference;
     private DatabaseReference mCommentsDatabaseReference;
-
     private String mEntertainmentKey;
     private String mEntertainmentName;
     private FirebaseAuth mFirebaseAuth;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<DataSnapshot> arr;
+    private DataSnapshot[] dataset;
 
 
     @Override
@@ -75,9 +90,10 @@ public class ViewEntertainmentActivity extends AppCompatActivity {
         mCommentorName = findViewById(R.id.commentorNameTextView);
         mCommentTextView = findViewById(R.id.commentMultiAutoCompleteTextView);
         mCommentorImage = findViewById(R.id.commentorProfileImageView);
-
+        mRecyclerView = findViewById(R.id.comment_recyclerView);
 
         // Initialize firebase
+        FirebaseApp.initializeApp(this);
         mFireBaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
         mUserDatabaseReference = mFireBaseDatabase.getReference().child("users");
@@ -92,6 +108,11 @@ public class ViewEntertainmentActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle(mEntertainmentName);
         }
+
+
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
         mLikesDatabaseReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -144,8 +165,10 @@ public class ViewEntertainmentActivity extends AppCompatActivity {
                         .load(photoUrl)
                         .into(mEntertainmentImage);
 
+                postComments();
+
                 // Display current user comments
-                mCommentsDatabaseReference.addValueEventListener(new ValueEventListener() {
+               /* mCommentsDatabaseReference.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                             //int i = 0;
@@ -184,21 +207,21 @@ public class ViewEntertainmentActivity extends AppCompatActivity {
                                 }
                             }
 
-                            /*for(int n=0; n<author.length; n++){
+                            for(int n=0; n<author.length; n++){
                                 mCommentorName.setText(author[i]);
                                 mCommentTextView.setText(comment[i]);
 
                                 Picasso.with(getApplicationContext())
                                         .load(image[i])
                                         .into(mCommentorImage);
-                            }*/
+                            }
 
                     }
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
                     }
-                });
+                });*/
             }
 
             @Override
@@ -284,4 +307,156 @@ public class ViewEntertainmentActivity extends AppCompatActivity {
         });
 
     }
+
+
+    public void postComments() {
+
+        //DatabaseReference mFirebaseDatabaseReference = mFireBaseDatabase.getReference();
+        mCommentsDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                arr = new ArrayList<>();
+
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    String eUID = (String) postSnapshot.child("entertainmentUID").getValue();
+                    if(mEntertainmentKey.equals(eUID) == true){
+                        arr.add(postSnapshot);
+                    }
+                }
+
+                if(arr.size() == 0){
+                    Snackbar sb = Snackbar.make(findViewById(R.id.viewEntertainment_activity), "No comments to display", Snackbar.LENGTH_LONG);
+                    View v = sb.getView();
+                    v.setBackgroundColor(ContextCompat.getColor(getApplication(), R.color.colorPrimary));
+                    sb.show();
+                }else {
+                    dataset = new DataSnapshot[arr.size()];
+                    for (int i = 0; i < arr.size(); i++) {
+                        dataset[i] = arr.get(i);
+                    }
+                    mAdapter = new MyAdapter(dataset);
+                    mRecyclerView.setAdapter(mAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
+
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private DataSnapshot[] mDataset;
+        FirebaseDatabase mFirebaseDatabase;
+        FirebaseAuth mFirebaseAuth;
+        DatabaseReference mLikesDatabaseReference;
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public CardView cardview;
+
+            public ViewHolder(CardView v) {
+                super(v);
+                cardview = v;
+                mFirebaseDatabase = FirebaseDatabase.getInstance();
+                mFirebaseAuth = FirebaseAuth.getInstance();
+                mLikesDatabaseReference = mFirebaseDatabase.getReference().child("Likes");
+                mLikesDatabaseReference.keepSynced(true);
+            }
+
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(DataSnapshot[] dataSnapshot) {
+            mDataset = dataSnapshot;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            // create a new view
+            CardView v = (CardView) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.activity_comments, parent, false);
+            // set the view's size, margins, paddings and layout parameters
+
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            // holder.mTextView.setText(mDataset[position]);
+            ((TextView) holder.cardview.findViewById(R.id.commentorNameTextView)).setText((position + 1) + ". " + mDataset[position].child("author").getValue());
+            ((TextView) holder.cardview.findViewById(R.id.commentMultiAutoCompleteTextView)).setText((String) mDataset[position].child("comment").getValue());
+
+            setPhotoUrl((String) mDataset[position].child("photoUrl").getValue(), holder);
+            //setLikeNumber(mDataset[position].getKey(), (TextView) holder.cardview.findViewById(R.id.likeCounterTextView));
+            //final int pos = position;
+            //final ImageView[] imageView = new ImageView[1];
+           /* holder.cardview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent eventIntent = new Intent(SearchableActivity.this, ViewEntertainmentActivity.class);
+                    eventIntent.putExtra("entertainmentName", (String) mDataset[pos].child("name").getValue());
+                    eventIntent.putExtra("entertainmentKey", mDataset[pos].getKey());
+                    startActivity(eventIntent);
+                }
+            }); */
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+        void setPhotoUrl(String image, ViewHolder vh){
+            ImageView event_image = vh.cardview.findViewById(R.id.commentorProfileImageView);
+            //final ProgressBar progressBar = vh.cardview.findViewById(R.id.imageProgressBar);
+            Picasso.with(event_image.getContext())
+                    .load(image)
+                    .into(event_image, new Callback() {
+                        @Override
+                        public void onSuccess() {
+                            //progressBar.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onError() {
+
+                        }
+                    });
+        }
+        void setLikeNumber(final String mEntertainmentKey, final TextView likesView){
+
+            mLikesDatabaseReference.child(mEntertainmentKey).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    likesView.setText(String.valueOf((int) dataSnapshot.getChildrenCount()));
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+
+    }
+
+
 }
