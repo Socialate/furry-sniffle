e803f9a85b56aafcb4a43f1ca772357b517bf94b
MusaRikhotso <1090294@students.wits.ac.za>
2017-10-09 16:35:34 +0200

    updated test

--- app/src/androidTest/java/com/socialteinc/socialate/ResetPasswordTest.java
+++ app/src/androidTest/java/com/socialteinc/socialate/ResetPasswordTest.java
@@ -11,6 +11,7 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
@@ -20,16 +21,19 @@ public class ResetPasswordTest{
  public ActivityTestRule<ResetPasswordActivity> rule = new ActivityTestRule<>(ResetPasswordActivity.class);
 
     @Test
-    public void checkEmailExistsTest(){
+    public void test1() throws InterruptedException{
+        onView(withId(R.id.resetPasswordButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkEmailExistsTest() throws InterruptedException{
 
         onView(withId(R.id.resetEmailEditText)).perform(typeText("valid@socialate.com"), closeSoftKeyboard());
 //        onView(withId(R.id.resetPasswordButton)).check(matches(isDisplayed()));
         onView(withId(R.id.resetPasswordButton)).check(matches(isClickable()));
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        Thread.sleep(5000);
+
 //        onView(withId(R.id.resetPasswordButton)).perform(click());
     }
 }
