c41257c96775cadd45e87793f601b09d4e655baf
MusaRikhotso <1090294@students.wits.ac.za>
2017-10-16 12:25:02 +0200

    updated tests

--- app/src/androidTest/java/com/socialteinc/socialate/ResetPasswordTest.java
+++ app/src/androidTest/java/com/socialteinc/socialate/ResetPasswordTest.java
@@ -1,5 +1,6 @@
 package com.socialteinc.socialate;
 
+import android.os.Looper;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
@@ -21,33 +22,40 @@ public class ResetPasswordTest{
 
     @Test
     public void test1() throws InterruptedException{
-        onView(withId(R.id.resetPasswordButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.resetEmailEditText)).check(matches(isDisplayed()));
         onView(withId(R.id.resetPasswordButton)).check(matches(isClickable()));
     }
 
     @Test
     public void test2() throws InterruptedException{
-
         onView(withId(R.id.resetEmailEditText)).perform(typeText("abcd@gmail.com"), pressBack());
         Thread.sleep(1000);
-        //onView(withId(R.id.resetPasswordButton)).perform(click());
-        //Thread.sleep(9000);
-        //onView(withId(R.id.resetPasswordConstraintLayout)).check(matches(isDisplayed()));
+
     }
 
     @Test
     public void checkEmailExistsTest() throws InterruptedException{
-
         onView(withId(R.id.resetEmailEditText)).perform(typeText("musa950820@gmail.com"), pressBack());
         Thread.sleep(1000);
-        //onView(withId(R.id.resetPasswordButton)).perform(click());
-        //Thread.sleep(9000);
-        //onView(withId(R.id.resetPasswordConstraintLayout)).check(matches(isDisplayed()));
-        //Thread.sleep(3000);
-        //onView(withId(R.id.resetPasswordConstraintLayout)).check(matches(isClickable()));
-        //onView(ViewMatchers.withId(R.id.resetPasswordConstraintLayout)).perform(ViewActions.openLinkWithText("LOGIN"));
-        //Thread.sleep(3000);
     }
 
-    
+    @Test
+    public void test3() throws InterruptedException{
+        Thread.sleep(1000);
+        onView(withId(R.id.resetEmailEditText)).perform(typeText("abcd@gmail.com"));
+        onView(withId(R.id.resetPasswordButton)).perform(click());
+        Thread.sleep(9000);
+        //onView(withId(R.id.resetPasswordConstraintLayout)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test4() throws InterruptedException{
+        Thread.sleep(1000);
+        onView(withId(R.id.resetEmailEditText)).perform(typeText("musa950820@gmail.com"));
+        onView(withId(R.id.resetPasswordButton)).perform(click());
+        Thread.sleep(9000);
+        //onView(withId(R.id.resetPasswordConstraintLayout)).check(matches(isDisplayed()));
+    }
+
+
 }
